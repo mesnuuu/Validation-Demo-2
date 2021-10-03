20fb3e20ccd437713ee0256ec26e7d31ed51b1ca
mesnuuu <mesnuuu@gmail.com>
2021-10-03 19:45:37 +0500

    Initial commit

---
+++ src/test/java/com/mesnu/validation/ValidationDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesnu.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
